7edb415654aaf781e0de7e256ebea131ccc38325
LEBLOIS VINCENT p1402522 <p1402522@univ-lyon1.fr>
2017-06-29 17:17:12 +0200

    Recuperations des opérations dans CalculatorTrainer depuis OperationsGenerator et non plus directement en dur dans la classe

--- SolidExercices/CalculatorTrainer.cs
+++ SolidExercices/CalculatorTrainer.cs
@@ -4,8 +4,8 @@ namespace SolidExercices
 {
     public class CalculatorTrainer
     {
-        private readonly string[] _operations = new[]
-            {"1+2,3", "2 x 3,6", "6-1-3,8", "6,6/3", "6/0", "not an operation", "a+1", "12", ""};
+        private static readonly OperationsGenerator _generator;
+        private readonly string[] _operations = _generator.Generate();
 
         public void Run()
         {
---
+++ SolidExercices/OperationsGenerator.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SolidExercices
+{
+    class OperationsGenerator
+    {
+        public string[] Generate()
+        {
+            return new[] {"1+2,3", "2 x 3,6", "6-1-3,8", "6,6/3", "6/0", "not an operation", "a+1", "12", ""};
+        }
+    }
+}
